ab7a6ad75ca928c7021310f4dd9d82b10004c61d
Nate Stuyvesant <nstuyvesant@gmail.com>
2018-08-02 07:45:46 -0400

    chore(admin): cleanup

--- admin/default.aspx.cs
+++ admin/default.aspx.cs
@@ -4,24 +4,20 @@ using System.Web.UI.WebControls;
 
 public partial class admin : System.Web.UI.Page
 {
-    protected void Page_Load(object sender, EventArgs e)
-    {
-        if (!Page.IsPostBack)
-        {
+    protected void Page_Load(object sender, EventArgs e) {
+        if (!Page.IsPostBack) {
         }
     }
 
-    protected void FindStudent_Click(object sender, ImageClickEventArgs e)
-    {
+    protected void FindStudent_Click(object sender, ImageClickEventArgs e) {
         gvStudents.DataBind();
     }
 
-    protected void dvSubscribers_ItemInserted(object sender, DetailsViewInsertedEventArgs e)
-    {
+    protected void dvSubscribers_ItemInserted(object sender, DetailsViewInsertedEventArgs e) {
         //gvSubscribers.DataBind();
     }
-    protected void FindSubscriber_Click(object sender, EventArgs e)
-    {
+
+    protected void FindSubscriber_Click(object sender, EventArgs e) {
         gvSubscribers.DataBind();
     }
 }
