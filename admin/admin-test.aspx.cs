41a4a13978a814a0959d38ed1d4b07cf24d203ae
Nate Stuyvesant <nstuyvesant@gmail.com>
2018-08-02 07:50:20 -0400

    chore(admin): initial commit

---
+++ admin/admin-test.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class adminTest : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e) {
+        if (!Page.IsPostBack) {
+        }
+    }
+
+    protected void FindStudent_Click(object sender, ImageClickEventArgs e) {
+        gvStudents.DataBind();
+    }
+
+    protected void dvSubscribers_ItemInserted(object sender, DetailsViewInsertedEventArgs e) {
+        //gvSubscribers.DataBind();
+    }
+
+    protected void FindSubscriber_Click(object sender, EventArgs e) {
+        gvSubscribers.DataBind();
+    }
+}
