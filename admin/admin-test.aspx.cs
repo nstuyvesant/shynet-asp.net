aef3da1ca2bd5471508d4fbc2702565df484a5f5
Nate Stuyvesant <nstuyvesant@gmail.com>
2018-08-03 23:10:36 -0400

    fix

--- admin/admin-test.aspx.cs
+++ admin/admin-test.aspx.cs
@@ -9,7 +9,7 @@ public partial class adminTest : System.Web.UI.Page
         }
     }
 
-    protected void FindStudent_Click(object sender, LinkButtonClickEventArgs e) {
+    protected void FindStudent_Click() {
         gvStudents.DataBind();
     }
 }
